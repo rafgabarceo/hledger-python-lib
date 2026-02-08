765b22633ec2b2f4a966742ed99d5ce4be7bce1c
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2026-02-08 17:01:26 +0800

    Added tests for builder

---
+++ tests/test_command_builder.py
@@ -0,0 +1,6 @@
+import pytest
+
+from hledger_python_lib.hledger import BaseHledgerCommandBuilder
+
+
+def test_builder(): ...
--- tests/test_ledger_file.py
+++ tests/test_ledger_file.py
@@ -1,3 +1,4 @@
+import csv
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from logging import DEBUG
@@ -7,6 +8,22 @@ import pytest
 from hledger_python_lib.hledger import BaseFile
 
 
-def test_initialization(random_hledger_csv, caplog):
-    caplog.set_level(DEBUG)
-    uut = LedgerFile(random_hledger_csv)
+def test_initialization(random_hledger_csv):
+    uut = BaseFile(random_hledger_csv)
+
+
+@pytest.mark.asycnio
+async def test_get_account_names(random_hledger_csv):
+    expected_headers = []
+    with open(random_hledger_csv, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for idx, row in enumerate(reader):
+            if idx == 0:
+                continue
+            expected_headers.append(row[4])
+    uut = BaseFile(random_hledger_csv)
+    headers = await uut.get_accounts()
+
+    available_headers = [header.get_name() for header in headers]
+
+    assert expected_headers.sort() == available_headers.sort()
